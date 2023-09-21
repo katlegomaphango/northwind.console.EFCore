b87446bce4fd10aa2a5a036853fda58eac2bf94e
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-21 12:29:24 +0200

    scafolding

--- northwind.EFCoreApp/northwind.EFCore/Program.cs
+++ northwind.EFCoreApp/northwind.EFCore/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Scaffold - DbContext "Data Source=.;Database=Northwind;TrustServerCertificate=True;Trusted_Connection=True;"
+//-provider Microsoft.EntityFrameworkCore.SqlServer - OutputDir Models - DataAnnotations - context NorthwindDb
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
