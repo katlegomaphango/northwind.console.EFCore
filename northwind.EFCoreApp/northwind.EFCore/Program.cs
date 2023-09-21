5a703f3392d40fb752010beabf3bddec0f70bc12
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-21 12:43:19 +0200

    data source menu

--- northwind.EFCoreApp/northwind.EFCore/Program.cs
+++ northwind.EFCoreApp/northwind.EFCore/Program.cs
@@ -10,3 +10,32 @@ builder.MultipleActiveResultSets = true;
 builder.Encrypt = true;
 builder.TrustServerCertificate = true;
 builder.ConnectTimeout = 10;
+
+WriteLine("Connect to:");
+WriteLine(" 1 - SQL Server on local machine");
+WriteLine(" 2 - Azure SQL Database");
+WriteLine(" 3 - Azure SQL Edge");
+WriteLine();
+Write("Press a key: ");
+
+ConsoleKey key = ReadKey().Key;
+WriteLine(); 
+WriteLine();
+
+if (key is ConsoleKey.D1 or ConsoleKey.NumPad1)
+{
+    builder.DataSource = ".";
+}
+else if (key is ConsoleKey.D2 or ConsoleKey.NumPad2)
+{
+    builder.DataSource = "tcp:apps-services-net7.database.windows.net,1433";
+}
+else if (key is ConsoleKey.D3 or ConsoleKey.NumPad3)
+{
+    builder.DataSource = "tcp:127.0.0.1,1433";
+}
+else
+{
+    WriteLine("No data source selected.");
+    return;
+}
