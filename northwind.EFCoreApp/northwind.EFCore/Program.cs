7d50e49c42c89796456a8a573efee20509809c57
katlegomaphango <mphangokatlego08@gmail.com>
2023-09-21 12:40:09 +0200

    initial builder

--- northwind.EFCoreApp/northwind.EFCore/Program.cs
+++ northwind.EFCoreApp/northwind.EFCore/Program.cs
@@ -1,5 +1,12 @@
 ﻿//Scaffold - DbContext "Data Source=.;Database=Northwind;TrustServerCertificate=True;Trusted_Connection=True;"
 //-provider Microsoft.EntityFrameworkCore.SqlServer - OutputDir Models - DataAnnotations - context NorthwindDb
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Data.SqlClient;
+
+SqlConnectionStringBuilder builder = new();
+
+builder.InitialCatalog = "Northwind";
+builder.MultipleActiveResultSets = true;
+builder.Encrypt = true;
+builder.TrustServerCertificate = true;
+builder.ConnectTimeout = 10;
